91ce9ff04f4aee0ab774ced9e2485d8a12020672
farid <faritfir521@gmail.com>
2025-05-16 14:47:45 +0700

    adding main.py and bencode decoder

---
+++ app/bencode_deoder/bencode_decoder.py
@@ -0,0 +1,81 @@
+class bencode_decoder:
+    @staticmethod
+    def string_decoder(bval, i) -> tuple:
+        # Check the format to ensure the current character is a digit
+        if i >= len(bval) and not chr(bval[i]).isdigit():
+            raise ValueError("Invalid bencode string format")
+        
+        # Get the string length
+        string_length = 0
+        while i < len(bval) and chr(bval[i]).isdigit():
+            string_length += int(bval[i:i+1])
+            i += 1
+
+        # Validate the format by checking for ':'
+        if not chr(bval[i]) == ":":
+            raise ValueError(f"Missing ':' at index {i}")
+
+        # Skip the ':'
+        i += 1
+
+        # Extract the string value and update the index
+        bencode_string_value = bval[i:i+string_length]
+        end_index = i + string_length
+        
+        return bencode_string_value, end_index
+    
+    @staticmethod
+    def integer_decoder(bval, i) -> tuple:
+        # Validate the format to ensure it starts with 'i'
+        if not chr(bval[i]) == "i":
+            raise ValueError(f"Invalid bencode integer format at index: {i}")
+        
+        # Skip the 'i'
+        i += 1
+
+        bencode_integer_value = b""
+        # Read until the 'e' character is found
+        while i < len(bval) and chr(bval[i]) != "e":
+            bencode_integer_value += bval[i:i+1]
+            i += 1
+        end_index = i + 1
+        
+        return bencode_integer_value, end_index
+        
+    @staticmethod
+    def list_decoder(bval, i) -> tuple:
+        bencode_list_value = []
+
+        # Validate the format by checking for 'l'
+        if i >= len(bval) or chr(bval[i]) != "l":
+            raise ValueError(f"Invalid format at index {i}")
+
+        i += 1  # Skip the 'l'
+
+        while i < len(bval):
+            # If 'e' is found, it marks the end of the list
+            if chr(bval[i]) == "e":
+                i += 1  # Skip the 'e'
+                return bencode_list_value, i
+
+            # Ensure the index does not go out of bounds before processing the next element
+            if i >= len(bval):
+                raise ValueError(f"Unexpected end of data at index {i}")
+
+            # Parse the element based on its type
+            value, i = bencode_decoder.parse_by_type(bval, i)
+            bencode_list_value.append(value)
+
+        # If the loop finishes without finding 'e', the format is invalid
+        raise ValueError("Missing 'e' at the end of list")
+
+    @staticmethod
+    def parse_by_type(bval, i) -> tuple:
+        # Parse the data based on its type
+        if chr(bval[i]).isdigit():
+            return bencode_decoder.string_decoder(bval, i)
+        elif chr(bval[i]) == "i":
+            return bencode_decoder.integer_decoder(bval, i)
+        else:
+            raise ValueError(f"Invalid bencode format at index: {i}")
+
