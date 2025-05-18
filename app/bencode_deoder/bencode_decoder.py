6fdb964375d6a6e9f57f07f878c150cc4986bb3e
farid <faritfir521@gmail.com>
2025-05-18 19:49:03 +0700

    adding decode method,fixing some bugs and adding main.py

--- app/bencode_deoder/bencode_decoder.py
+++ app/bencode_deoder/bencode_decoder.py
@@ -48,11 +48,12 @@ class bencode_decoder:
 
         # Validate the format by checking for 'l'
         if i >= len(bval) or chr(bval[i]) != "l":
-            raise ValueError(f"Invalid format at index {i}")
+            raise ValueError(f"Invalid format at index {i} value: {chr(bval[i])}")
 
         i += 1  # Skip the 'l'
 
         while i < len(bval):
+
             # If 'e' is found, it marks the end of the list
             if chr(bval[i]) == "e":
                 i += 1  # Skip the 'e'
@@ -72,22 +73,32 @@ class bencode_decoder:
     @staticmethod
     def dictionaries_decoder(bval,i) -> tuple:
         bencode_dictionaries_value = {}
-    
+        
         # validating the dictionaries format by ensuring it contains 'd'
         if chr(bval[i]) != "d":
-            raise ValueError(f"invalid bencode dictionaries format: missing 'e'\nat index: {i}")
+            raise ValueError(f"Invalid bencode dictionaries format: missing 'd' at index {i}")
         
-        i += 1 # skipping 'd'
+        i += 1 # skip 'd'
 
+        # extracting dictionaries values
         while i < len(bval) and chr(bval[i]) != "e":
-            key,i = bencode_decoder.parse_by_type(bval,i)
-            value,i = bencode_decoder.parse_by_type(bval,i)
-            bencode_dictionaries_value[key] = value
+            key, i = bencode_decoder.parse_by_type(bval, i)
+    
+            # Validating <key:value> format
+            if chr(bval[i]) == "e":
+                raise ValueError("dictionaries must be in <key:value> format")
+
+            value, i = bencode_decoder.parse_by_type(bval, i)
+            bencode_dictionaries_value[key] = value        
+    
+        # memastikan terdapat "e" di akhir dictionaries
+        if chr(bval[i]) != "e":
+            raise ValueError("missing 'e' at the end of bencode dictionaries")
         
+
+
         end_index = i + 1
-
         return bencode_dictionaries_value, end_index
-
 
     @staticmethod
     def parse_by_type(bval, i) -> tuple:
@@ -96,7 +107,23 @@ class bencode_decoder:
             return bencode_decoder.string_decoder(bval, i)
         elif chr(bval[i]) == "i":
             return bencode_decoder.integer_decoder(bval, i)
+        elif chr(bval[i]) == "l":
+            return bencode_decoder.list_decoder(bval,i)
+        elif chr(bval[i]) == "d" :
+            return bencode_decoder.dictionaries_decoder(bval,i) 
         else:
-            raise ValueError(f"Invalid bencode format at index: {i}")
+            raise ValueError(f"Invalid bencode format at index: {i} value: {chr(bval[i])}")
 
-print(bencode_decoder.dictionaries_decoder(b"d4:datai55ee",0))
+    @staticmethod
+    def decode(bval,i=0) -> tuple:
+        results = []
+        
+        while i < len(bval):
+            val,i = bencode_decoder.parse_by_type(bval,i)
+            results.append(val)
+
+        return results,i
+
+
+print(bencode_decoder.decode(b"i999e5:hellod4:datai888ee"))
+
--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
 import json
 import sys
+from bencode_deoder.bencode_decoder import bencode_decoder
 
+
+def main():
+    command = sys.argv[1]
+    if command == "decode":
+        bencode = sys.argv[2].encode()
+
+        values = bencode_decoder.decode(bencode)
+        print(values)
+
+if __name__ == "__main__":
+    main()
